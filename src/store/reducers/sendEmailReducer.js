b744b344dd5e6e540211e1336eb5a0628c5d0938
Avik Panda <avikpanda@Aviks-MacBook-Air.local>
2024-01-25 03:31:29 +0530

    [AVA-020] Fix TTS Delay

--- src/store/reducers/sendEmailReducer.js
+++ src/store/reducers/sendEmailReducer.js
@@ -4,6 +4,12 @@ const initialState = {
 
 export default function sendEmailReducer(state = initialState, action) {
   switch (action.type) {
+    case "SEND_EMAIL":
+      return { ...state, status: "In-Progress" };
+    case "SEND_EMAIL_SUCCESS":
+      return { ...state, status: "Success" };
+    case "SEND_EMAIL_ERROR":
+      return { ...state, status: "Error" };
     case "RESET_REDUCERS":
       return initialState;
     default:
